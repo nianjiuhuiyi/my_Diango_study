3d58905c5ec119886121ec64159f0f83d8b71227
songh_work <songh-cc@foxmail.com>
2022-04-26 17:19:00 +0800

    cookie，登录用户名记忆

--- booktest/urls.py
+++ booktest/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     re_path("book/(\d+)/", views.hero_info),   # (\d+) 正则表达式的组的值才会被传给函数当另外的参数
     path("book/create/", views.create),   # 添加一本固定的书
     re_path("book/delete/(\d+)/", views.delete),  # 添加书的删除按钮
+
     path("login/", views.login),  # 登录页面
-    path("login_check/", views.login_check)  # 登录检查
+    path("login_check/", views.login_check),  # 登录检查
+
+    path("set_cookie", views.set_cookie),
+    path("get_cookie", views.get_cookie),
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -89,15 +89,21 @@ def delete(request, book_id):
     return redirect("/index/book")  # 跟上面效果一模一样，推荐这个吧
 
 
+# /login
 def login(request):
     """显示登录页面"""
-    return render(request, "booktest/login.html")
+    # 下面login_check在登录成功时保存了用户名的cookie，这里去取来判断，有就直接先填上
+    # userName = request.POST.get("userName")  # 注意前面别写错了，用的是cookie
+    userName = request.COOKIES.get("userName")  # 当做字典来用的
+    replace_data = dict()
+    if userName is not None:
+        replace_data["userName"] = userName
+    return render(request, "booktest/login.html", replace_data)
 
 
+# /login_check
 def login_check(request):
     """
-    request.POST
-    request.GET    这俩都是<class 'django.http.request.QueryDict'>类型
     request传过来的对象的数据都在这个里面
     """
     print(type(request.POST))   # 类型是<class 'django.http.request.QueryDict'>
@@ -107,9 +113,47 @@ def login_check(request):
     # 1.获取提交的用户名和密码 (html中的input标签中的name值是什么，这里的key就是什么)
     userName = request.POST.get("userName")
     password = request.POST.get("password")
+
     # 2.数据库查询用户名、密码进行校验，这里就是模拟一下
     if userName == "admin" and password == "123":
-        # 成功，那就跳转到一个页面
-        return redirect("/index/123/")
+        # # 成功，那就跳转到一个页面 (这是前面简单的代码)
+        # return redirect("/index/123/")
+
+        # 以下是只记住登录成功的用户名
+        response = redirect("/index/123/")  # 这个对象本质也是HttpResponse的
+        # 如果勾选了复选框，返回的就是字符串“on”，没勾选返回的就是一个None
+        remember = request.POST.get("my_remember")
+        if remember is not None:
+            response.set_cookie("userName", userName)  # 关闭浏览器即过期，方便调试
+            # response.set_cookie("userName", userName, max_age=14*24*3600)
+        return response
     else:
         return HttpResponse("错误！")
+
+
+"""开始cookie的学习"""
+
+# /set_cookie
+def set_cookie(request):
+    """设置cookie信息"""
+    response = HttpResponse("设置cookie")
+    # 设置一个cookie信息，名字为num，值为1，，
+    # .set_cookie是HttpResponse对象带有的方法
+    # response.set_cookie("num", 123)
+    # response.set_cookie("num_new", 456)  # 可设置多个cookie
+    # 设置过期时间，两种方式(别不设置14天过期)
+    # 方式一：
+    response.set_cookie("num", 123, max_age=14*24*3600)  # max_age是到期时间剩余秒数
+    # 方式二：
+    from datetime import datetime, timedelta
+    response.set_cookie("num_new", 456789, expires=datetime.now() + timedelta(days=14))
+
+    return response  # 返回给浏览器
+
+
+# /get_cookie
+def get_cookie(request):
+    """浏览器发过来后，获取cookie信息"""
+    num = request.COOKIES["num"]
+    # 获取到前面自己设的cookie值，然后返回给页面
+    return HttpResponse(num)
